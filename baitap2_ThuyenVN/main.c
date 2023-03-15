2af3ee14b819195f04771a098bfaa50ca9f2dd50
DangLHb <haidanglai23@gmail.com>
2023-03-15 20:40:35 +0700

    DangLHb add file main.c

---
+++ baitap2_ThuyenVN/main.c
@@ -0,0 +1,14 @@
+/*Bai 2:
+
+
+1. Cài đặt lại thuật toán Insertion Sort với CTDL là double linked list.
+2. Cài đặt 1 số thuật toán đơn giản sau bằng đệ quy:
+- Tìm max trong mảng
+- Tính tổng các số trong mảng
+3. Vẫn tìm max / tổng bằng đệ quy nhưng cho cấu trúc dữ liệu là linked list
+*/
+int main()
+{
+	
+	return 0;
+}
